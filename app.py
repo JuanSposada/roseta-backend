0ca606b61dba81531a65aac1502f663589a741c1
Aspoda <ssmoreda@gmail.com>
2024-10-13 22:22:36 -0700

    Se logro las comunicacion con la base de datos, se creo el modelo de la tabla usuarios, se utilizo marshmallow para serializar los datos obtenidos y mostrarlos en json

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Text, BigInteger
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/test'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tester:test@localhost/test'
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 with app.app_context():
@@ -24,9 +27,11 @@ def sensores():
 def camaras():
     return jsonify(message='camara')
 
-@app.route("/api/users")
+@app.route("/api/users", methods=['GET'])
 def users():
-    return jsonify(message='users')
+    users_list = Usuario.query.all()
+    result = usuarios_schema.dump(users_list)
+    return jsonify(result)
 
 @app.route("/api/users/<string:id>")
 def users_id(id: str):
@@ -43,6 +48,26 @@ def wifi():
 @app.route('/api/not-found')
 def not_found():
     return jsonify(message='request not found'), 404
+
+# modelos de base de datos
+class Usuario(db.Model): 
+    __tablename__ = 'usuarios_test'
+
+    id_usuario = Column(BigInteger, primary_key=True)
+    nombre = Column(Text, nullable=True)
+    correo = Column(Text, unique=True, nullable=True)
+    password = Column(Text, nullable=True )
+    telefono = Column(Text, nullable=True)
+
+# Clase para marshmallow
+
+class UsuarioSchema(ma.Schema):
+    class Meta:
+        fields = ('id_usuario', 'nombre', 'correo', 'password', 'telefono')
+
+usuario_schema = UsuarioSchema()
+usuarios_schema = UsuarioSchema(many=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
