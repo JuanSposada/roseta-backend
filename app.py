f012a52a4ccd45829227ab963aa1706fd3f05171
Aspoda <ssmoreda@gmail.com>
2024-11-03 21:22:39 -0800

    Se completo implementacion de Vercel con postgress, pruebas de coneccion y de insecion de datos por medio de la api arrojaron resultados positivos

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from routes import  register_blueprints
 from flask_smorest import Api
 from routes.usuarios_route import usuarios_bp
 import os 
-
-
+from sqlalchemy import text
 
 app = Flask(__name__)
 
@@ -17,26 +16,45 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config['OPENAPI_URL_PREFIX'] = '/'
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tester:test@localhost/test'
+
+db_uri = os.environ.get('POSTGRES_URL', 'postgresql://tester:test@localhost/test')
+
+# Corrige el prefijo de la URI si es necesario
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
+@app.route('/test-db-connection')
+def test_db_connection():
+    try:
+        # Ejecuta una consulta simple para probar la conexión
+        result = db.session.execute(text("SELECT 1"))
+        return "Conexión exitosa a la base de datos", 200
+    except Exception as e:
+        return f"Error de conexión: {e}", 500
 
 api = Api(app)
 api.register_blueprint(usuarios_bp, url_prefix='/api')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 
 db.init_app(app)
 ma.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
 
 #registrar blueprints
 register_blueprints(api)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Text, BigInteger, Boolean, ForeignKey,TIMESTAMP, Integer
 
-
 db = SQLAlchemy()
 
 class Usuario(db.Model): 
